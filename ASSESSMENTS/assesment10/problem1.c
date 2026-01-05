e3344b54c3dde6357e7e25d4932a36db483ef6c2
Chandrukrishnakumar <chandrukrishna82197@gmail.com>
2026-01-05 11:14:39 +0530

    eleven

---
+++ ASSESSMENTS/assesment10/problem1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Original value: %d\n", num);
+
+    num = num + 1;   
+
+    printf("Changed value: %d\n", num);
+
+    return 0;
+}
